70643aec2e5efc5228a5463679dc8f947fa1e52e
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-08-05 15:17:58 +0530

    Create BookDonateScreen-stylsheet.js

---
+++ BookDonateScreen-stylsheet.js
@@ -0,0 +1,21 @@
+const styles = StyleSheet.create({
+  subContainer:{
+    flex:1,
+    fontSize: 20,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  button:{
+    width:100,
+    height:30,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:"#ff5722",
+    shadowColor: "#000",
+    shadowOffset: {
+       width: 0,
+       height: 8
+     }
+  }
+})
+© 2021 GitHu
